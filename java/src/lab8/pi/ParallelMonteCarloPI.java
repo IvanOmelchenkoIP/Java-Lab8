e52d88c99ff9188f688d5fa8840395fdd4cff121
IvanOmelchenkoIP <io25112002@gmail.com>
2022-12-22 15:52:12 +0200

    Add CountDownLatch
    Add CountDownLatch to replace joins to allow threads to actually run
    parallel

--- java/src/lab8/pi/ParallelMonteCarloPI.java
+++ java/src/lab8/pi/ParallelMonteCarloPI.java
@@ -1,20 +1,25 @@
 package lab8.pi;
 
+import java.util.concurrent.CountDownLatch;
 
 public class ParallelMonteCarloPI {
 
 	private final int ITERATIONS = 100000000;
 	
 	public double count(int threads) throws InterruptedException {
+		CountDownLatch countdown = new CountDownLatch(threads);
 		int threadIterations = Math.round(ITERATIONS / threads);
 		SyncPoints syncPoints = new SyncPoints();
-		ThreadPointCounter counter = new ThreadPointCounter(threadIterations, syncPoints);
+		//ThreadPointCounter counter = new ThreadPointCounter(threadIterations, syncPoints);
 		for (int i = 0; i < threads; i++) {
-			Thread thread = new Thread(counter);
-			thread.start();
-			thread.join();
+			new Thread(new ThreadPointCounter(threadIterations, syncPoints, countdown)).start();
 		}
-		return (syncPoints.getPoints() / Double.valueOf(ITERATIONS)) * 4;
+		try {
+			countdown.await();
+			return (syncPoints.getPoints() / Double.valueOf(ITERATIONS)) * 4;
+		} catch(InterruptedException exception) {
+			throw exception;
+		}
 	}
 	
 	public int iterations() {
--- java/src/lab8/pi/ThreadPointCounter.java
+++ java/src/lab8/pi/ThreadPointCounter.java
@@ -1,6 +1,6 @@
 package lab8.pi;
 
-import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class ThreadPointCounter implements Runnable {
 
@@ -8,12 +8,15 @@ public class ThreadPointCounter implements Runnable {
 
 	private final int iterations;
 	private final SyncPoints syncPoints;
+	
+	private final CountDownLatch countdown;
 
-	public ThreadPointCounter(int iterations, SyncPoints pPoints) {
+	public ThreadPointCounter(int iterations, SyncPoints pPoints, CountDownLatch countdown) {
 		this.iterations = iterations;
 		this.syncPoints = pPoints;
+		this.countdown = countdown;
 	}
-
+	
 	@Override
 	public void run() {
 		int points = 0;
@@ -21,13 +24,13 @@ public class ThreadPointCounter implements Runnable {
 		for (int i = 0; i < iterations; i++) {
 			double x = Math.random();
 			double y = Math.random();
-			//if (points % 100000 == 0) System.out.println(Thread.currentThread().getName());
+			if (points % 100000 == 0) System.out.println(Thread.currentThread().getName());
 			if (x*x + y*y < distance) points += 1;  
 			/*if (Math.pow(Math.random(), 2) + Math.pow(Math.random(), 2) <= Double.valueOf(CENTER_DISTANCE)) {
 				syncPoints.add(1);
 			}*/
 		}
 		syncPoints.add(points);
+		countdown.countDown();
 	}
-
 }
